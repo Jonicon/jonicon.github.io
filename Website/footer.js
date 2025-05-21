68522e23317a747bec5c09d405aca2cd6ae27d8f
Joni Kontusalmi <joni.kontusalmi@student.laurea.fi>
2025-05-21 11:42:56 +0300

    Website assignement

---
+++ Website/footer.js
@@ -0,0 +1,40 @@
+document.write('<footer class="text-muted p-3">\
+  <div class="container-sm px-5">\
+    <div class="row">  \
+      <div class="col-sm-3 text-center">\
+        <p>\
+          Home of a finnish miniature artist Joni Kontusalmi.\
+        </p>\
+      </div>\
+      <div class="col-sm-3">\
+        <ul>\
+          <li class="list-group-item">+358 40 7541797</li>\
+          <li class="list-group-item"><a href="mailto:info@ascetic.fi">Email</a></li>\
+          <li class="list-group-item"><p>Hansatie 30 A 5, 02780 Espoo</p></li>\
+        </ul>\
+      </div>\
+      <div class="col-sm-2">\
+        <ul>\
+          <li class="list-group-item">\
+            <a href="contact.html">Contact</a>\
+          </li>\
+          <li class="list-group-item">\
+            <a href="privacy.html">Privacy</a>\
+          </li>\
+          <li class="list-group-item">\
+            <a href="about.html">About me</a>\
+          </li>\
+          <li class="list-group-item">\
+            <a href=""></a>\
+          </li>\
+        </ul>\
+      </div>\
+      <div class="col-sm-4">\
+        <img src="images/portrait.jpg" class= "rounded-circle" width="20%" alt="In the grim\ darkness of the far future, there is only war">\
+      </div>\
+  </div>\
+  </div>\
+  <div>\
+    <p class="text-center">&copy;Joni Kontusalmi</p>\
+  </div>\
+</footer>');
---
+++ Website/navigation.js
@@ -0,0 +1,19 @@
+document.write ('<nav class="navbar navbar-expand-md bg-light">\
+        <div class="container-fluid"> \
+            <ul class="navbar-nav"> \
+                <div class="navbar-header"> \
+                    <a class="navbar-brand" href="index.html"> \
+                        <img src=images/logo.png height="40"> \
+                    </a> \
+                </div> \
+                <li class="nav-item"> \
+                    <a class="nav-link" href="#">Gallery</a> \
+                </li> \
+                <li class="nav-item"> \
+                    <a class="nav-link" href="contact.html">Contact</a> \
+                </li> \
+            </ul> \
+        </div> \
+    </nav>');
+
+    // document.getElementById('navigation').innerHTML = html;
