1eee1e3ca016f6826ebd5e0739d8d7b06f6dfedd
Joni Kontusalmi <joni.kontusalmi@student.laurea.fi>
2025-05-21 14:38:50 +0300

    priv nav icon

--- Website/navigation.js
+++ Website/navigation.js
@@ -1,4 +1,5 @@
-document.write ('<nav class="navbar navbar-expand-md bg-light">\
+document.write ('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">\
+    <nav class="navbar navbar-expand-md bg-light">\
         <div class="container-fluid"> \
             <ul class="navbar-nav"> \
                 <div class="navbar-header"> \
@@ -12,6 +13,9 @@ document.write ('<nav class="navbar navbar-expand-md bg-light">\
                 <li class="nav-item"> \
                     <a class="nav-link" href="contact.html">Contact</a> \
                 </li> \
+                <li class="nav-item">\
+                    <a class="nav-link" href="https://www.instagram.com/asceticminiatures"> <i  class="bi bi-instagram style="font-size: 2rem;"></i> </a>\
+                </li>\
             </ul> \
         </div> \
     </nav>');
