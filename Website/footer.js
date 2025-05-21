0845945350905be07a55cd9b3650e0c8f158d97f
Joni Kontusalmi <joni.kontusalmi@student.laurea.fi>
2025-05-21 12:33:50 +0300

    favicon updates

--- Website/footer.js
+++ Website/footer.js
@@ -1,5 +1,5 @@
-document.write('<footer class="text-muted p-3">\
-  <div class="container-sm px-5">\
+document.write('<footer class="text-muted">\
+  <div class="container-fluid  bg-light">\
     <div class="row">  \
       <div class="col-sm-3 text-center">\
         <p>\
@@ -10,7 +10,8 @@ document.write('<footer class="text-muted p-3">\
         <ul>\
           <li class="list-group-item">+358 40 7541797</li>\
           <li class="list-group-item"><a href="mailto:info@ascetic.fi">Email</a></li>\
-          <li class="list-group-item"><p>Hansatie 30 A 5, 02780 Espoo</p></li>\
+          <li class="list-group-item">Hansatie 30 A 5, 02780 Espoo</li>\
+          <li class="list-group-item">Y-tunnus: 3319248-3</li>\
         </ul>\
       </div>\
       <div class="col-sm-2">\
@@ -35,6 +36,6 @@ document.write('<footer class="text-muted p-3">\
   </div>\
   </div>\
   <div>\
-    <p class="text-center">&copy;Joni Kontusalmi</p>\
+    <p class="text-center bg-light">&copy;Joni Kontusalmi</p>\
   </div>\
 </footer>');
